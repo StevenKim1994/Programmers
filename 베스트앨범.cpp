95be7f8a2c4da2f5104e116d95a51728078b548d
Si Yun (Steven Kim) <dev@donga.ac.kr>
2020-06-09 23:19:05 +0900

    update

--- 베스트앨범.cpp
+++ 베스트앨범.cpp
@@ -13,41 +13,80 @@ using namespace std;
  * 장르 내에서 재생 횟수가 같은 노래 중에서는 고유번호가 낮은 노래먼저 수록
  */
 
-bool compare(pair<string,int> a, pair<string,int> b)
+struct aa
 {
-	return a.second < b.second;
-}
+	string first;
+	int second;
+	bool operator<(const aa &c) const
+	{
+		return second > c.second;
+	}
+};
+
+struct bb
+{
+	int first;
+	int second;
+	bool operator<(const bb &c) const
+	{
+		if(second==c.second)
+		{
+			return first < c.first;
+		}
+		return second > c.second;
+	}
+};
 
 vector<int> solution(vector<string> genres, vector<int> plays)
 {
-	vector<int> result;
+	vector<int> answer;
 	int size = genres.size();
-	
-	unordered_map<string, int> myMap;
-	vector<pair<string,int>> vec;
-	
-	// 재생된 장르 누적시키기 많이 재생된 장르를 찾기 위함
+
+	unordered_map<string, int> hash;
+	vector <aa> vec;
+
 	for(int i = 0; i<size; i++)
 	{
-		myMap[genres.at(i)] += plays.at(i);
+		hash[genres[i]] += plays[i];
 	}
 
-	for(auto pair:myMap)
+	for (auto pair : hash)
 	{
-		vec.push_back(make_pair(pair.first, pair.second));
+		vec.push_back({pair.first,pair.second});
 	}
 
 	sort(vec.begin(), vec.end());
 
-	for (int i = 0; i < vec.size(); i++)
-		cout << vec.at(i).first << vec.at(i).second << endl;
+	for(auto iter : vec)
+	{
+		vector<bb> tmp;
+		for(int i = 0; i<size;i++)
+		{
+			if(iter.first == genres[i])
+			{
+				tmp.push_back({i,plays[i]});
+			}
+		}
+		
+		sort(tmp.begin(), tmp.end());
 
-	// 많이 재생된 장르는 찾음 이제 여기서 장르중에서 재생크기 큰거 순으로 result에 push_back 시키자!
 
 	
-	return result;
-}
+	
 
+		//
+		int cnt = 0;
+		for (auto t : tmp)
+		{
+			cnt++;
+			if (cnt > 2) break;
+			answer.push_back(t.first);
+			cout << t.first << endl;
+		}
+	}
+	
+	return answer;
+}
 int main()
 {
 	vector<string> genres;
@@ -70,11 +109,6 @@ int main()
 
 	result = solution(genres, plays);
 	
-	for(int i = 0; i<result.size(); i++)
-	{
-		cout << result[i] << endl;
-	}
-
 
 	return 0;
 }
