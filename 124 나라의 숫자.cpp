cb8584f8d892859446b35d2cdbcc3b61956abd12
Si Yun (Steven Kim) <dev@donga.ac.kr>
2020-06-15 14:53:26 +0900

    #academy
    update

---
+++ 124 나라의 숫자.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+string solution(int n)
+{
+	string answer;
+
+	return answer;
+}
+
+
+int main()
+{
+
+	int n;
+
+	cin >> n;
+
+	cout << solution(n) << endl;
+
+
+
+	return 0;
+}
